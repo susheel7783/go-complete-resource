2df18a904163423d8357f34c718f8197118ffd72
Susheel Kumar <97793079+susheel7783@users.noreply.github.com>
2026-01-11 17:32:41 +0530

    Update pointers.go

--- code/04-understanding-pointers/02-pointers-as-values/pointers.go
+++ code/04-understanding-pointers/02-pointers-as-values/pointers.go
@@ -60,3 +60,13 @@ func getAdultYears(age int) int {
 // ptr = &age             // & = get address of age
 // value := *ptr          // * = get value at address (dereference)
 // *ptr = 40              // Change value through pointer
+
+
+// A Pointer's Null Value
+// All values in Go have a so-called "Null Value" - i.e., the value that's set as a default if no value is assigned to a variable.
+
+// For example, the null value of an int variable is 0. Of a float64, it would be 0.0. Of a string, it's "".
+
+// For a pointer, it's nil - a special value built-into Go.
+
+// nil represents the absence of an address value - i.e., a pointer pointing at no address / no value in memory.
